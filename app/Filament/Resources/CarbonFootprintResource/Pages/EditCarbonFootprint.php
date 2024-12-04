6cab1bf4faaa392a34e32dd0d72437089bda42b7
mo <125622355+Janyapon-jpsm@users.noreply.github.com>
2024-12-04 17:39:20 +0700

    Before adding new tables

---
+++ app/Filament/Resources/CarbonFootprintResource/Pages/EditCarbonFootprint.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\CarbonFootprintResource\Pages;
+
+use App\Filament\Resources\CarbonFootprintResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditCarbonFootprint extends EditRecord
+{
+    protected static string $resource = CarbonFootprintResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
